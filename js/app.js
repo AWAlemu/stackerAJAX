c6801325d46c265a064785e77430d8ebbd6e70eb
Alex Weldetsadik <alexweldetsadik@gmail.com>
2016-07-19 10:38:13 -0400

    Set stackexchange protocol from http to https

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ var getUnanswered = function(tags) {
 	};
 	//Returns deferred object
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: "https://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
 		type: "GET",
@@ -112,7 +112,7 @@ var getTopAnswerers = function(answerers) {
 	}
 
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/tags/" + answerers + "/top-answerers/all_time" ,
+		url: "https://api.stackexchange.com/2.2/tags/" + answerers + "/top-answerers/all_time" ,
 		data: request,
 		dataType: "jsonp",
 		type: "GET",
